5f7f5770b1ff9d79cf50a996be43f8e00f92d5ef
Lisa44750 <153626463+Lisa44750@users.noreply.github.com>
2026-07-17 20:51:51 +0200

    Add files via upload

---
+++ vueMonProfil.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * --------------
+ * vueMonProfil
+ * --------------
+ * 
+ * @version 07/2021 par NB : intégration couche modèle objet
+ * 
+ * Variables transmises par le contrôleur detailResto contenant les données à afficher : 
+  ---------------------------------------------------------------------------------------- */
+/** @var Utilisateur  $util utilisteur à afficher */
+
+/** @var array $mesRestosAimes  */
+/** @var int $idU  */
+/** @var string $mailU  */
+/**
+ * Variables supplémentaires :  
+  ------------------------- */
+/** @var Resto $unResto */
+
+?>
+
+<h1>Mon profil</h1>
+
+Mon adresse électronique : <?= $util->getMailU() ?> <br />
+Mon pseudo : <?= $util->getPseudoU() ?> <br />
+
+<hr>
+
+les restaurants que j'aime : <br />
+<?php
+foreach ($mesRestosAimes as $unResto) {
+    ?>
+    <a href="./?action=detail&idR=<?= $unResto->getIdR() ?>"><?= $unResto->getNomR() ?></a><br />
+    <?php
+}
+?>
+    
+<hr>
+    
+Les types de cuisine que j'aime : <br />
+<?php 
+foreach ($mestypesAimes as $unResto){
+    ?>
+    <a href=''<?= $unResto->getIdR() ?>"><?= $unResto->getNomR() ?>/>
+    <?php
+}
+?>
+<hr>
